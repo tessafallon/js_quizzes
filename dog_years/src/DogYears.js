6a9a3fd4190671b601c7202bcf5cf5c24658c068
contessa <tessa.fallon@gmail.com>
2014-01-22 14:57:16 -0500

    dog years

--- dog_years/src/DogYears.js
+++ dog_years/src/DogYears.js
@@ -1,2 +1,18 @@
 //create an object called dog
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
+
+function Dog(){
+	this.age = 3;
+}
+
+Dog.prototype.ageInYears = function(){
+	return this.age;
+};
+
+Dog.prototype.ageInDays = function(){
+	return this.age * 365;
+};
+
+Dog.prototype.ageInDogYears = function(){
+	return this.age * 7;
+};
